bd8ba80797997ce2f5f3fb6b6d60facab97be5df
Rahul Agrawal <rahulagrawal0310@gmail.com>
2024-05-19 06:34:01 +0530

    Problem 3

---
+++ src/main/scala/LargestContinuousSum.scala
@@ -0,0 +1,26 @@
+object LargestContinuousSum {
+  def main(args: Array[String]): Unit = {
+    val arr: Array[Int] = Array(2, -1, 2, 2, -4, 2, 1, -3, 5)
+
+    println("Largest continuous sum - " + findLargestContinuousSumN(arr))
+  }
+
+  private def findLargestContinuousSumN(arr: Array[Int]): Int = {
+    var curr_sum = 0
+    var global_sum = 0
+    for (i <- arr.indices) {
+      if(arr(i) < 0 ) {
+        if (curr_sum >= global_sum) {
+          global_sum = curr_sum
+          curr_sum = 0
+        }
+      } else
+        curr_sum = curr_sum + arr(i)
+    }
+    if (curr_sum >= global_sum)
+      global_sum = curr_sum
+
+    global_sum
+  }
+
+}
